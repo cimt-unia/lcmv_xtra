9691616c4a8c48eb216431d66eddb2c358e33454
ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>
2026-02-08 16:12:41 +0100

    Update __init__.py
    Signed-off-by: ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>

--- lcmv_xtra/__init__.py
+++ lcmv_xtra/__init__.py
@@ -1,16 +1,21 @@
 # lcmv_xtra/__init__.py
 
-from ._config import *  # Set env vars first
+from ._config import *  
 
-# Main workflow functions (public API)
+# Main workflow functions 
 from .source_estimation import execute_source_estimation
-from .utils import download_fsaverage  # ← Keep this in utils where it belongs
+from .utils import download_fsaverage  
 from .atlas_extraction import gt_extraction, difumo_extraction
 from .connectivity import (
     compute_gt_motor_connectivity,
     compute_gt_full_connectivity,
     compute_difumo_connectivity,
     get_motor_roi_metadata
+)
+from .viz import (  
+    visualize_source_at_coordinate,
+    plot_mni_orthoview,
+    compute_psd
 )
 
 __version__ = "0.1.0"
@@ -22,6 +27,8 @@ __all__ = [
     "compute_gt_motor_connectivity",
     "compute_gt_full_connectivity", 
     "compute_difumo_connectivity",
-    "get_motor_roi_metadata"
+    "get_motor_roi_metadata",
+    "visualize_source_at_coordinate",  
+    "plot_mni_orthoview",
+    "compute_psd"
 ]
-
