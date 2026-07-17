2892c253400700a72a46350e6ab819caa38631a4
ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>
2026-07-17 21:44:55 +0200

    Create custom_tensor.py
    Signed-off-by: ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>

---
+++ notebooks/custom_tensor.py
@@ -0,0 +1,35 @@
+# Create custom tensor
+
+import lcmv_xtra as lx
+from pathlib import Path
+import pandas as pd
+
+FS_DIR = Path("/derivatives/_fs")
+OUTPUT_DIR = Path("/eeg_tensor")
+PROJECT_BASE = Path("xtra")
+CLEAN_DIR = Path("/eeg_clean")
+
+DBS_ROIS = {
+    "R1": [10.958, 5.563, -4.948],
+    "R2": [28.304, 24.100, -17.535],
+    "L1": [-15.983, 6.077, 0.027],
+    "L2": [-14.719, -2.180, -0.865],
+}
+
+for condition in ["gain", "loss"]:
+    df = pd.DataFrame([{
+        "subject_id": "DP02",
+        "fif_path": str(CLEAN_DIR / f"DP02_{condition}_eeg_raw_eeg.fif")
+    }])
+    
+    lx.assemble_custom_tensor(
+        data_index=df,
+        fs_dir=FS_DIR,
+        output_dir=OUTPUT_DIR,
+        roi_coordinates=DBS_ROIS,
+        task_name=condition,  # Just "gain" or "loss"
+        project_base=PROJECT_BASE,
+        mode="single",
+        n_jobs=1,
+        verbose=True
+    )
